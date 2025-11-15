861f1017868be30c5d8ddb5685cf4214e5f3d0bd
Simon Girard <simon.girard1991@gmail.com>
2025-11-15 15:31:46 +0100

    first commit: project skeleton

---
+++ src/main/java/org/girardsimon/wealthpay/WealthpayApplication.java
@@ -0,0 +1,13 @@
+package org.girardsimon.wealthpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WealthpayApplication.class, args);
+    }
+
+}
